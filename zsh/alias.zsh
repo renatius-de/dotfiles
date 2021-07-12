945c19bfc157593174f3917ddb5017443479282f
René Six <info@renatius.de>
2021-07-12 15:22:00 +0200

    Add aliases explicit for ZSH

---
+++ zsh/alias.zsh
@@ -0,0 +1,15 @@
+# Global aliases
+alias -g G='|& egrep --ignore-case'
+alias -g N='&> /dev/null'
+
+# remap the buildin commads
+alias which='whence -vas'
+alias where='whence -cas'
+
+# no spell correction for cp, mv, rm, mkdir, rmdir and adding default options
+alias cp='nocorrect cp'
+alias mv='nocorrect mv'
+alias ln='nocorrect ln'
+alias mkdir='nocorrect mkdir'
+
+which keychain >/dev/null 2>&1 && alias kadd="keychain --timeout $((60 * 12)) ~/.ssh/keys/**/id*~*.pub"
