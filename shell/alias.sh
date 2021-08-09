54b03ab1fc2b59bff7e224c76d23cef97d8c1953
René Six <info@renatius.de>
2021-08-09 08:52:18 +0200

    Remove 'kadd' alias from shells

--- shell/alias.sh
+++ shell/alias.sh
@@ -63,7 +63,6 @@ if which keychain >/dev/null 2>&1; then
   alias keychain="keychain --systemd"
 
   # shellcheck disable=SC2139
-  alias kadd="keychain --timeout $((60 * 12))"
   alias kclear="keychain --clear"
 fi
 
