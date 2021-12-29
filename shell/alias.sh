bc68e8482b40ef8136cb7bd952e996b63fbce475
René Six <info@renatius.de>
2021-12-29 23:09:14 +0100

    Simplify shell configuration

--- shell/alias.sh
+++ shell/alias.sh
@@ -10,13 +10,7 @@ alias ls="ls --color=auto --classify --dereference-command-line-symlink-to-dir -
 
 alias l="ls --human-readable --numeric-uid-gid -l --time-style=+'%Y-%m-%d %H:%m'"
 alias la="ls --almost-all"
-alias lB="la --ignore-backups"
-alias lD="la --directory"
-alias llD="ll --directory"
-alias li="la --inode"
 alias ll="l --almost-all"
-alias lr="ll --reverse -t"
-alias lZ="la --context"
 
 alias rm="rm --interactive=once --verbose"
 alias rm="rm --interactive=once --verbose"
@@ -26,26 +20,15 @@ alias md="mkdir --mode=0700 --parents --verbose"
 alias chgrp="chgrp --changes"
 alias chmod="chmod --changes"
 alias chown="chown --changes"
-alias du="du --human-readable --one-file-system --time --time-style=+'%Y-%m-%d' --total"
-alias df="df --human-readable --local --print-type"
 alias cp="cp -av"
 alias grep="grep --color=auto"
 
-which wget >/dev/null 2>&1 && alias wget="wget --tries=10 --continue --timeout=30 --wait=30"
-which htop >/dev/null 2>&1 && alias top="htop"
-which locate >/dev/null 2>&1 && alias locate="locate --ignore-case --existing --regex"
-which bc >/dev/null 2>&1 && alias bc="bc -l"
 which lsattr >/dev/null 2>&1 && alias lsattr="lsattr -a"
 which chattr >/dev/null 2>&1 && alias chattr="chattr -R"
 
 if which rsync >/dev/null 2>&1; then
   alias rsync="rsync --recursive --links --perms --times --owner --group --devices --specials --hard-links --whole-file --delete --cvs-exclude --prune-empty-dirs --compress --stats --human-readable --progress"
   alias rsync_fat="rsync --chmod='u=rwX,go=' --size-only"
-fi
-
-if which pwgen >/dev/null 2>&1; then
-  alias pwgen="pwgen -cns"
-  alias pgen="pwgen 30 1"
 fi
 
 if which nvim >/dev/null 2>&1; then
@@ -67,11 +50,4 @@ if which keychain >/dev/null 2>&1; then
 
   # shellcheck disable=SC2139
   alias kclear="keychain --clear"
-fi
-
-if which docker-compose >/dev/null 2>&1; then
-  alias dcb="docker-compose build --force-rm --pull"
-  alias dcd="docker-compose down --remove-orphans --volumes"
-  alias dcda="dcd --rmi all"
-  alias dcu="docker-compose up --abort-on-container-exit"
 fi
