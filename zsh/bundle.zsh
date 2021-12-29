bd6a7c59188541d5180d4fb666e297351613e3e1
René Six <info@renatius.de>
2021-12-30 00:47:16 +0100

    Remove unused zsh plugin

--- zsh/bundle.zsh
+++ zsh/bundle.zsh
@@ -7,18 +7,12 @@ if [[ -r ~/.zsh/plugin/antigen.zsh ]]; then
 
     # bundles from the default repo (robbyrussell's oh-my-zsh).
     antigen bundle docker
-    antigen bundle editor
     antigen bundle gpg-agent
-    antigen bundle history-substring-search
     antigen bundle keychain
-    antigen bundle pass
-    antigen bundle rvm
     antigen bundle sdk
     antigen bundle ssh-agent
     antigen bundle themes
-    antigen bundle tmuxinator
     antigen bundle vi-mode
-    antigen bundle zsh_reload
 
     # add bundles from ush users
     antigen bundle zsh-users/zsh-autosuggestions
@@ -27,7 +21,6 @@ if [[ -r ~/.zsh/plugin/antigen.zsh ]]; then
     antigen bundle zsh-users/zsh-syntax-highlighting
 
     # bundles from misc repos
-    antigen bundle jreese/zsh-titles
     antigen bundle lukechilds/zsh-nvm
 
     antigen theme candy-kingdom
