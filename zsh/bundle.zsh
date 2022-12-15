9f7ad84f2b2a0b47f8a85bb7361a923f5bcc7ba5
René Six <info@renatius.de>
2022-12-15 19:24:49 +0100

    Reconfigure ZSH

--- zsh/bundle.zsh
+++ zsh/bundle.zsh
@@ -1,29 +1,44 @@
-if [[ -r ~/.zsh/plugin/antigen.zsh ]]; then
+if [[ -r ~/.zsh/plugin/antigen/antigen.zsh ]]; then
     # shellcheck disable=SC1090
-    source ~/.zsh/plugin/antigen.zsh
+    source ~/.zsh/plugin/antigen/antigen.zsh
 
     # load the oh-my-zsh's library.
     antigen use oh-my-zsh
 
     # bundles from the default repo (robbyrussell's oh-my-zsh).
-    antigen bundle docker
-    antigen bundle keychain
+    antigen bundle brew
+    which docker >/dev/null 2>&1 && antigen bundle docker
+    which docker-compose >/dev/null 2>&1 && antigen bundle docker-compose
+    which git >/dev/null 2>&1 && antigen bundle git
+    which gradle >/dev/null 2>&1 && antigen bundle gradle
+    which helm >/dev/null 2>&1 && antigen bundle helm
+    antigen bundle history-substring-search
+    which keychain >/dev/null 2>&1 && antigen bundle keychain
+    which kubectl >/dev/null 2>&1 && antigen bundle kubectl
+    which kubectl >/dev/null 2>&1 && antigen bundle kubectx
+    antigen bundle macos
+    which mvn >/dev/null 2>&1 && antigen bundle mvn
+    antigen bundle nvm
+    antigen bundle rvm
+    which sbt >/dev/null 2>&1 && antigen bundle sbt
     antigen bundle sdk
     antigen bundle ssh-agent
+    which terraform >/dev/null 2>&1 && antigen bundle terraform
     antigen bundle themes
-    antigen bundle tmuxinator
+    which tig >/dev/null 2>&1 && antigen bundle tig
+    which tmux >/dev/null 2>&1 && antigen bundle tmux
+    which tmuxinator >/dev/null 2>&1 && antigen bundle tmuxinator
     antigen bundle vi-mode
 
     # add bundles from ush users
     antigen bundle zsh-users/zsh-autosuggestions
     antigen bundle zsh-users/zsh-completions
-    antigen bundle zsh-users/zsh-history-substring-search
     antigen bundle zsh-users/zsh-syntax-highlighting
 
     # bundles from misc repos
     antigen bundle lukechilds/zsh-nvm
 
-    antigen theme candy-kingdom
+    antigen theme candy
 
     # tell antigen that you're done.
     antigen apply
--- zsh/export.zsh
+++ zsh/export.zsh
@@ -1,16 +1,15 @@
 if which nvim >/dev/null 2>&1; then
   EDITOR="nvim"
+  VISUAL="nvim -R"
+  FCEDIT="nvim"
 elif which vim >/dev/null 2>&1; then
   EDITOR="vim"
+  VISUAL="vim -R"
+  FCEDIT="vim"
 fi
 export EDITOR="${EDITOR:-vi}"
-
-if which nvim >/dev/null 2>&1; then
-  VISUAL="nvim -R"
-elif which vim >/dev/null 2>&1; then
-  VISUAL="vim -R"
-fi
 export VISUAL="${VISUAL:-${EDITOR}}"
+export FCEDIT="${FCEDIT:-${EDITOR}}"
 
 export MANWIDTH="72"
 export TAR_OPTIONS="--auto-compress --delay-directory-restore --exclude-backups --exclude-caches --no-overwrite-dir --numeric-owner --totals"
@@ -33,3 +32,42 @@ export LANG=en_US.UTF-8
 export LANGUAGE=en_GB:en_US:en
 
 unset LC_ALL
+
+export CDPATH=".:~"
+export DIRSTACKSIZE="32"
+export HISTFILE=~/.cache/zsh/history
+export HISTSIZE="10000"
+export LINES="200"
+export LISTMAX="${LINES:200}"
+export REPORTTIME="300"
+export SAVEHIST="${HISTSIZE:100}"
+if [[ ${TERM} == linux ]]; then
+  readonly TMOUT=300
+  export TMOUT
+fi
+unset LOGCHECK
+unset MAIL
+unset MAILCHECK
+unset MAILPATH
+
+typeset -U path
+
+[[ -d /sbin ]]           && path=($path /sbin)
+[[ -d /usr/sbin ]]       && path=($path /usr/sbin)
+[[ -d /usr/local/sbin ]] && path=($path /usr/local/sbin)
+[[ -d /bin ]]            && path=($path /bin)
+[[ -d /usr/bin ]]        && path=($path /usr/bin)
+[[ -d /usr/local/bin ]]  && path=($path /usr/local/bin)
+
+# set PATH so it includes user's private bin if it exists
+[[ -d ~/bin ]] && path=(~/bin $path)
+
+# add rvm to path
+[[ -d ~/.rvm/bin ]]                && path=(~/.rvm/bin $path)
+[[ -d ~/.rvm/rubies/default/bin ]] && path=(~/.rvm/rubies/default/bin $path)
+
+# add local rubygems installs
+if which ruby > /dev/null 2>&1 && which gem > /dev/null 2>&1; then
+    [[ -d "$(ruby -e 'puts Gem.user_dir')"/bin ]] && path=("$(ruby -e 'puts Gem.user_dir')"/bin $path)
+fi
+export PATH
