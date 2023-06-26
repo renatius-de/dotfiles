cec98308a40aa4c3a7e42906e5932ef328489c02
René Six <rene.six@flavia-it.de>
2023-06-26 11:22:56 +0200

    Cleanup zsh bundles

--- zsh/bundle.zsh
+++ zsh/bundle.zsh
@@ -6,29 +6,29 @@ if [[ -r ~/.zsh/plugin/antigen/antigen.zsh ]]; then
     antigen use oh-my-zsh
 
     # bundles from the default repo (robbyrussell's oh-my-zsh).
-    antigen bundle brew
+    antigen bundle history-substring-search
+    antigen bundle nvm
+    antigen bundle rvm
+    antigen bundle sdk
+    antigen bundle themes
+    antigen bundle vi-mode
+
+    if [ "$(uname)" = "Darwin" ];then
+      antigen bundle macos
+      antigen bundle brew
+    fi
+
     which docker >/dev/null 2>&1 && antigen bundle docker
     which docker-compose >/dev/null 2>&1 && antigen bundle docker-compose
     which git >/dev/null 2>&1 && antigen bundle git
     which gradle >/dev/null 2>&1 && antigen bundle gradle
     which helm >/dev/null 2>&1 && antigen bundle helm
-    antigen bundle history-substring-search
-    which keychain >/dev/null 2>&1 && antigen bundle keychain
     which kubectl >/dev/null 2>&1 && antigen bundle kubectl
     which kubectl >/dev/null 2>&1 && antigen bundle kubectx
-    antigen bundle macos
     which mvn >/dev/null 2>&1 && antigen bundle mvn
-    antigen bundle nvm
-    antigen bundle rvm
     which sbt >/dev/null 2>&1 && antigen bundle sbt
-    antigen bundle sdk
-    antigen bundle ssh-agent
+    which ssh-agent >/dev/null 2>&1 && antigen bundle ssh-agent
     which terraform >/dev/null 2>&1 && antigen bundle terraform
-    antigen bundle themes
-    which tig >/dev/null 2>&1 && antigen bundle tig
-    which tmux >/dev/null 2>&1 && antigen bundle tmux
-    which tmuxinator >/dev/null 2>&1 && antigen bundle tmuxinator
-    antigen bundle vi-mode
 
     # add bundles from ush users
     antigen bundle zsh-users/zsh-autosuggestions
