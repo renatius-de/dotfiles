c54ced5d1f735722bfb5c7d0d24a8db1cd53d6f0
René Six <info@renatius.de>
2024-08-15 09:07:47 +0200

    Updates and simplifies the ZSH alias configuration

--- zsh/alias.zsh
+++ zsh/alias.zsh
@@ -6,42 +6,36 @@ alias ln="nocorrect ln"
 alias mkdir="nocorrect mkdir"
 alias mv="nocorrect mv"
 
-if which dircolors > /dev/null 2>&1; then
-  alias ls="ls --color=auto --classify --dereference-command-line-symlink-to-dir --hide-control-chars --sort=version"
-  alias l="ls --human-readable --numeric-uid-gid -l --time-style=+'%Y-%m-%d %H:%m'"
-  alias la="ls --almost-all"
-  alias ll="l --almost-all"
+alias ls="ls -@FG"
+alias l="ls -hln"
+alias la="ls -A"
+alias ll="l -A"
 
-  alias chgrp="chgrp --changes"
-  alias chmod="chmod --changes"
-  alias chown="chown --changes"
-  alias cp="cp -av"
-  alias md="mkdir --mode=0700 --parents --verbose"
-  alias rd="rmdir --parents --ignore-fail-on-non-empty"
-  alias rm="rm --interactive=once --verbose"
-  alias rmdir="rmdir --verbose"
-else
-  alias ls="ls -@FG"
-  alias l="ls -hln"
-  alias la="ls -A"
-  alias ll="l -A"
-
-  alias rm="rm -iv"
-  alias md="mkdir -m 0700 -pv"
-  alias rd="rmdir -p"
-fi
+alias rm="rm -iv"
+alias md="mkdir -m 0700 -pv"
+alias rd="rmdir -p"
 
 alias grep="grep --color=auto"
 
-which lsattr >/dev/null 2>&1 && alias lsattr="lsattr -a"
-which chattr >/dev/null 2>&1 && alias chattr="chattr -R"
+if which docker-compose > /dev/null 2>&1; then
+  alias dc="docker compose"
 
-if which mvn >/dev/null 2>&1; then
-  alias mvn="mvn --errors --fail-fast --update-snapshots"
-  alias mka="mvn clean install site"
+  alias dcb="dc build"
+  alias dcdn="dc down --remove-orphans --volumes"
+  alias dcl="dc logs --timestamps"
+  alias dclf="dcl --follow"
+  alias dcps="dc ps --all"
+  alias dcup="dc up --build --detach --quiet-pull --remove-orphans --wait"
 fi
 
-if which nvim >/dev/null 2>&1; then
+if which mvn > /dev/null 2>&1; then
+  alias mvn="mvn --errors --fail-fast --update-snapshots"
+
+  alias mka="mvn clean install site"
+  alias mkd="mvn -P dev"
+fi
+
+if which nvim > /dev/null 2>&1; then
   alias vi="nvim"
   alias view="nvim -R"
   alias vim="nvim"
