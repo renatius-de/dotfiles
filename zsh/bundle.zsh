78e517bf0d29be15e2619adb1010adeea2e22246
René Six <info@renatius.de>
2025-08-16 21:41:23 +0200

    Refactor zsh configuration: modularize Antigen bundle loading, improve macOS handling, and enhance readability

--- zsh/bundle.zsh
+++ zsh/bundle.zsh
@@ -1,29 +1,38 @@
-if [[ -r ~/.zsh/plugin/antigen/antigen.zsh ]]; then
-    # shellcheck disable=SC1090
-    source ~/.zsh/plugin/antigen/antigen.zsh
+# Antigen-Setup mit gebündelten, gut strukturierten Bundles
+ANTIGEN_FILE="${ANTIGEN_FILE:-${ZDOTDIR:-$HOME}/.zsh/plugin/antigen/antigen.zsh}"
 
-    # load the oh-my-zsh's library.
-    antigen use oh-my-zsh
+if [[ -r "$ANTIGEN_FILE" ]]; then
+  # shellcheck disable=SC1090
+  source "$ANTIGEN_FILE"
 
-    # bundles from the default repo (robbyrussell's oh-my-zsh).
-    antigen bundle history-substring-search
-    antigen bundle themes
-    antigen bundle vi-mode
+  antigen use oh-my-zsh
 
-    if [ "$(uname)" = "Darwin" ];then
-      antigen bundle brew
-      antigen bundle macos
+  load_antigen_bundles() {
+    local b
+    for b in "$@"; do
+      antigen bundle "$b"
+    done
+  }
 
-      export HOMEBREW_NO_ENV_HINTS=1
-    fi
+  local -a COMMON_BUNDLES=(
+    history-substring-search
+    jenv
+    themes
+    pyenv
+    vi-mode
+  )
+  load_antigen_bundles "${COMMON_BUNDLES[@]}"
 
-    # add bundles from ush users
-    antigen bundle zsh-users/zsh-autosuggestions
-    antigen bundle zsh-users/zsh-completions
-    antigen bundle zsh-users/zsh-syntax-highlighting
+  if [[ "$(uname -s)" == "Darwin" ]]; then
+    load_antigen_bundles brew macos
+    export HOMEBREW_NO_ENV_HINTS=1
+  fi
 
-    antigen theme candy
+  load_antigen_bundles \
+    zsh-users/zsh-autosuggestions \
+    zsh-users/zsh-completions \
+    zsh-users/zsh-syntax-highlighting
 
-    # tell antigen that you're done.
-    antigen apply
+  antigen theme candy
+  antigen apply
 fi
