fc26a7b36bd351bf2978bd5b02b47e3ac1fbde74
René Six <info@renatius.de>
2026-05-11 09:35:18 +0200

    feat: enhance Neovim configuration with additional options for clipboard, mouse, and scrolling

--- vim/init.lua
+++ vim/init.lua
@@ -3,13 +3,14 @@ vim.cmd([[syntax enable]])
 vim.cmd([[colorscheme evening]])
 
 local opt = vim.opt
-opt.autoindent = true
 opt.background = "dark"
 opt.backspace = "indent,eol,start"
 opt.backup = false
 opt.cindent = true
+opt.clipboard = "unnamedplus"
 opt.cmdwinheight = 5
 opt.colorcolumn = "+1"
+opt.completeopt = { "menu", "menuone", "noselect" }
 opt.confirm = true
 opt.cursorcolumn = true
 opt.cursorline = true
@@ -26,22 +27,31 @@ opt.joinspaces = false
 opt.laststatus = 2
 opt.lazyredraw = true
 opt.linebreak = true
+opt.mouse = "a"
 opt.number = true
+opt.path:append("**")
 opt.previewheight = 5
+opt.scrolloff = 8
+opt.sidescrolloff = 8
 opt.shiftround = true
 opt.shiftwidth = 4
 opt.showmatch = true
 opt.showtabline = 2
+opt.shortmess:append("c")
+opt.signcolumn = "yes"
 opt.smartindent = true
 opt.smarttab = true
 opt.softtabstop = 4
 opt.spellsuggest = { "best", 10 }
 opt.splitbelow = true
 opt.splitright = true
+opt.swapfile = false
+opt.termguicolors = true
+opt.undodir = vim.fn.stdpath("cache") .. "/undo"
+opt.undofile = true
 opt.visualbell = true
+opt.writebackup = false
 
 if vim.fn.has("spell") == 1 then
   opt.spelllang = { "de_de", "de", "en_gb", "en_us", "en" }
 end
-
-vim.g.python3_host_prog = "/usr/bin/python3"
